227ab1891dff2e4e721ebe6c927c1a670533e36e
Szu-Hsuan, Wu <shuan@csie.io>
2019-12-31 12:56:15 +0800

    更新 ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,42 +11,32 @@ module.exports = {
       mocha: true
     },
     extends: ['eslint:recommended'],
-    // check if imports actually resolve
-    settings: {
-      'import/resolver': {
-        webpack: {
-          config: 'build/webpack.base.conf.js'
-        }
-      }
-    },
-    // add your custom rules here
     rules: {
-      // don't require .vue extension when importing
       'import/extensions': ['error', 'always', {
         js: 'never',
         vue: 'never'
       }],
-      'no-param-reassign': ['error', { props: false }],
-      // disallow reassignment of function parameters
-      // disallow parameter object manipulation except for specific exclusions
-      /*'no-param-reassign': ['error', {
+      'no-param-reassign': ['error', {
         props: true,
         ignorePropertyModificationsFor: [
-          'state', // for vuex state
-          'acc', // for reduce accumulators
-          'e' // for e.returnvalue
+          'state',  // for vuex state
+          'acc',    // for reduce accumulators
+          'e'       // for e.returnvalue
         ]
-      }],*/
-      // allow optionalDependencies
-      /*'import/no-extraneous-dependencies': ['error', {
-        optionalDependencies: ['test/unit/index.js']
-      }],*/
+      }],
       'import/no-extraneous-dependencies': 'off',
       'import/extensions': 'off',
-      // allow debugger during development
       'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
       'no-console': 'off',
       'linebreak-style': ['error', 'unix'],
+      'no-unused-vars': ['error', { 
+        'vars': 'all', 
+        'args': 'none', 
+        'ignoreRestSiblings': false 
+      }],
+      'no-constant-condition': ['error', {
+        'checkLoops': false 
+      }],
       indent: ['warn', 2],
       quotes: ['error', 'single'],
       semi: ['error', 'always']
